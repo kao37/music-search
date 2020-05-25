b4c01339b60cf353c00e4bc1cc50e9b3977004d7
kao37 <4107937@gmail.com>
2020-05-25 10:36:12 +0900

    music search-app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+<v-parallax
+    height="300"
+    src="https://www.u4get.com/wp-content/uploads/2019/02/TE-D01d_model-1140x641.jpg"
+  ></v-parallax>
+<!-- https://i.ytimg.com/vi/vsFi9jPnQ9E/maxresdefault.jpg -->
+
+    <v-row class="text-center">
+      <v-col
+        class="mb-1"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          音楽検索
+        </h2>
+          <v-text-field
+            v-model="keyword"
+            :counter= "50"
+            placeholder="アーティスト名を入れてね"
+            required
+          ></v-text-field>
+            <v-btn rounded color="pink darken-1" dark @click="search">-検索-</v-btn>
+        </v-col>
+    </v-row>
+  </v-container>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      keywprd: '',
+    }
+  },
+  methods:{
+    search(){
+      this.$router.push(`/result/${this.keyword}`)
+    }
   }
-}
+};
 </script>
